a2491d014f6d97af115a1822f4182511079170a8
Victor Lee <myleevictor@gmail.com>
2018-06-22 15:22:50 +0800

    refactor method and params names to camelCase

--- findMostCommon.go
+++ findMostCommon.go
@@ -1,57 +1,55 @@
 package main
 
 import (
-    "fmt"
-    "regexp"
-    s "strings"
-    "sort"
+	"fmt"
+	"regexp"
+	"sort"
+	s "strings"
 )
 
-func get_words_from(text string) []string{
-    words:= regexp.MustCompile("\\w+")
-    return words.FindAllString(s.ToLower(text), -1)
+func getWordsFrom(text string) []string {
+	words := regexp.MustCompile("\\w+")
+	return words.FindAllString(s.ToLower(text), -1)
 }
 
-func count_words (words []string) map[string]int{
-    word_counts := make(map[string]int)
-    for _, word :=range words{
-        word_counts[word]++
-    }
-    return word_counts;
+func countWords(words []string) map[string]int {
+	wordCounts := make(map[string]int)
+	for _, word := range words {
+		wordCounts[word]++
+	}
+	return wordCounts
 }
 
-func console_out (ordered_word_counts PairList){
-    for _, word_count :=range ordered_word_counts{
-        fmt.Printf("%v\n", word_count)
-    }
+func consoleOut(orderedWordCounts PairList) {
+	for _, wordCount := range orderedWordCounts {
+		fmt.Printf("%v\n", wordCount)
+	}
 }
 
-
-func rankByWordCount(wordFrequencies map[string]int) PairList{
-  pl := make(PairList, len(wordFrequencies))
-  i := 0
-  for k, v := range wordFrequencies {
-    pl[i] = Pair{k, v}
-    i++
-  }
-  sort.Sort(sort.Reverse(pl))
-  return pl
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
 }
 
 type Pair struct {
-  Key string
-  Value int
+	Key   string
+	Value int
 }
 
 type PairList []Pair
 
-func (p PairList) Len() int { return len(p) }
+func (p PairList) Len() int           { return len(p) }
 func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
-func (p PairList) Swap(i, j int){ p[i], p[j] = p[j], p[i] }
-
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func main() {
-    text := "To be, I am learning Go! go is a nice, nice, nice, nice, nice, language to learn so, so, so, so, so, so, so, as, as, as, as, as, to become best programmer. I got this from a book and it is my input to this test"
-console_out(rankByWordCount(count_words(get_words_from(text))))  
+	text := "To be, I am learning Go! go is a nice, nice, nice, nice, nice, language to learn so, so, so, so, so, so, so, as, as, as, as, as, to become best programmer. I got this from a book and it is my input to this test"
+	consoleOut(rankByWordCount(countWords(getWordsFrom(text))))
 
 }
