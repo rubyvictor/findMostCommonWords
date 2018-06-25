4d32f0b614ecbb75b469ecb89193871a993e4727
Victor Lee <myleevictor@gmail.com>
2018-06-25 09:59:12 +0800

    fixed bug in sort

--- findMostCommon.go
+++ findMostCommon.go
@@ -33,7 +33,7 @@ func rankByWordCount(wordFrequencies map[string]int) PairList {
 		pl[i] = Pair{k, v}
 		i++
 	}
-	sort.Sort(sort.Reverse(pl))
+	sort.Sort(pl)
 	slicedPl := pl[0:10]
 	return slicedPl
 }
@@ -46,7 +46,7 @@ type Pair struct {
 type PairList []Pair
 
 func (p PairList) Len() int           { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func main() {
